7d6e970f0ebd352f6a4dd3933c57de3ab62efecb
Helmsfeld <someone@mymachine>
2019-04-12 10:44:50 +0200

    Python 3 combatibility

--- seek_and_destroy.py
+++ seek_and_destroy.py
@@ -1,42 +1,69 @@
-def phi_inverse(t):
+import sys
+import itertools
+
+
+def z_to_zz(z):
     n = 0
-    while n*(n+1) / 2 <= t:
+    while n * (n + 1) // 2 <= z:
         n += 1
     n -= 1
-    time = n
-    strategy_nr = t - (n*(n+1)/2)
-    time -= strategy_nr
+    x = z - (n * (n + 1) // 2)
+    y = n - x
 
-    return strategy_nr, time
+    return x, y
 
-def get_v_and_c(i):
-    c = i % 4
-    vabs = 1 + i / 4
-    if c == 0:
-        return -vabs, '>'
-    if c == 1:
-        return vabs, '>'
-    if c == 2:
-        return vabs, '<'
-    if c == 3:
-        return -vabs, '<'
 
 def gt(t, v):
-    return t*(v+1)
+    return t * (v + 1)
+
 
 def lt(t, v):
     return -gt(t, v)
 
-def route(c):
-    if c == '>': return gt
-    if c == '<': return lt
+
+def get_v_and_c(i):
+    c = i % 6
+    vabs = 1 + i // 6
+    if c == 0:
+        return 0, gt
+    if c == 1:
+        return -vabs, gt
+    if c == 2:
+        return vabs, gt
+    if c == 3:
+        return 0, lt
+    if c == 4:
+        return vabs, lt
+    if c == 5:
+        return -vabs, lt
+
 
 def shoot_at(t):
-    strategy_nr, time = phi_inverse(t)
+    strategy_nr, time = z_to_zz(t)
     v, c = get_v_and_c(strategy_nr)
-    wanted_shot = route(c)(time, v)
-    fixed_shot = wanted_shot + v * (t-time)
+    wanted_shot = c(time, v)
+    fixed_shot = wanted_shot + v * (t - time)
     return fixed_shot, v, c, wanted_shot, time
 
-for i in range(100):
-    print(i, shoot_at(i))
+
+if len(sys.argv) == 3:
+    x = sys.argv[1]
+    v = sys.argv[2]
+else:
+    x = 413
+    v = 5
+
+print("Position: {}".format(x))
+print("Velocity: {}".format(v))
+
+# for i in range(10000):
+# print(i, shoot_at(i)[0])
+
+for t in itertools.count():
+    xt = x + v * t
+    shot = shoot_at(t)
+    i = shot[0]
+    # print(xt, shot)
+    if xt == i:
+        print("Found at t={}".format(t))
+        break
